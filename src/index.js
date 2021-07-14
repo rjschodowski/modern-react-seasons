7a2c0fa3d0ffc6cd6707ce90a6d1b02ae907f728
R.J <rjschodowski@hotmail.com>
2021-07-14 14:22:49 -0400

    error message prompt added with state

--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ class App extends React.Component {
     constructor(props) {
         super(props);
             //This is the only time we do direct assignment to this.state b/c it is inside a constructor
-        this.state = { lat: null };
+        this.state = { lat: null, errorMessage: '' };
         
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
                 this.setState({ lat: position.coords.latitude });
             },
-            (err) => console.log(err)
+            (err) => {
+                this.setState({ errorMessage: err.message });
+            }
         );
     }
     
@@ -19,7 +21,13 @@ class App extends React.Component {
     //React says we have to define render
 
     render() {
-        return <div>Latitude: {this.state.lat}</div>
+        return (
+        <div>
+            Latitude: {this.state.lat}
+            <br />
+            Error: {this.state.errorMessage}
+            </div>
+            );
     }
 }
 
