aaa160eafaa4d5f53b35cd39db8e682e249a0510
R.J <rjschodowski@hotmail.com>
2021-07-15 12:17:41 -0400

    code refactor (componentDidMount lifecycle method added)

--- src/index.js
+++ src/index.js
@@ -2,21 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-            //This is the only time we do direct assignment to this.state b/c it is inside a constructor
-        this.state = { lat: null, errorMessage: '' };
-        
+    state = { lat: null, errorMessage: '' };
+    
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({ lat: position.coords.latitude });
-            },
-            (err) => {
-                this.setState({ errorMessage: err.message });
-            }
+            (position) => this.setState({ lat: position.coords.latitude }),
+            (err) => this.setState({ errorMessage: err.message })
         );
     }
-    
+
 
     //React says we have to define render
 
